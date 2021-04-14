753c1bb6a158d5fcab869419b3c8410be4da5752
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-13 21:06:16 -0700

    Implemented S3 file uploads

--- Class Work/petes-pets/public/javascripts/scripts.js
+++ Class Work/petes-pets/public/javascripts/scripts.js
@@ -2,25 +2,30 @@ if (document.querySelector("#new-pet")) {
   document.querySelector("#new-pet").addEventListener("submit", (e) => {
     e.preventDefault()
 
-    let pet = {}
-    const inputs = document.querySelectorAll(".form-control")
-    for (const input of inputs) {
-      pet[input.name] = input.value
-    }
+    // Use FormData to grab everything now that we have files mixed in with text
+    var form = document.getElementById("new-pet")
+    var pet = new FormData(form)
 
     axios
-      .post("/pets", pet)
+      .post("/pets", pet, {
+        headers: {
+          "Content-Type": "multipart/form-data;",
+        },
+      })
       .then(function (response) {
-        console.log(response)
-        console.log(response.data)
-        window.location.replace(`/pets/${response.data._id}`)
+        window.location.replace(`/pets/${response.data.pet._id}`)
       })
       .catch(function (error) {
         const alert = document.getElementById("alert")
+        console.log(error)
         alert.classList.add("alert-warning")
         alert.textContent =
           "Oops, something went wrong saving your pet. Please check your information and try again."
         alert.style.display = "block"
+        setTimeout(() => {
+          alert.style.display = "none"
+          alert.classList.remove("alert-warning")
+        }, 3000)
       })
   })
 }
