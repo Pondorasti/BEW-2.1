3055813c6b7ec4a941fe7e769d6d88c874fbdf9b
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-21 16:29:28 -0700

    Add minimal chatting functionality

--- Class Work/make-chat/public/index.js
+++ Class Work/make-chat/public/index.js
@@ -1,16 +1,40 @@
 $(document).ready(() => {
   const socket = io.connect()
+  let currentUsername
 
+  // Join Chat
   $("#create-user-btn").click((event) => {
     event.preventDefault()
     const username = $("#username-input").val()
     if (username.length > 0) {
+      currentUsername = username
       socket.emit("new user", username)
       $(".username-form").remove()
+      $(".main-container").css("display", "flex")
     }
   })
 
   socket.on("new user", (username) => {
     console.log(`${username} has joined the chat!`)
+    $(".users-online").append(`<div class="user-online">${username}</div>`)
+  })
+
+  // Send Messages
+  $("#send-chat-btn").click((event) => {
+    const message = $("#chat-input").val()
+    if (message.length > 0) {
+      socket.emit("new message", { sender: currentUsername, message })
+    }
+
+    $("#chat-input").val("")
+  })
+
+  socket.on("new message", ({ sender, message }) => {
+    $(".message-container").append(`
+      <div class="message">
+        <p class="message-user">${sender}: </p>
+        <p class="message-text">${message}</p>
+      </div>
+    `)
   })
 })
--- Class Work/make-chat/sockets/chat.js
+++ Class Work/make-chat/sockets/chat.js
@@ -1,6 +1,13 @@
 module.exports = (io, socket) => {
+  // New User
   socket.on("new user", (username) => {
     console.log(`${username} has joined the chat!`)
     io.emit("new user", username)
+  })
+
+  // New Message
+  socket.on("new message", ({ sender, message }) => {
+    console.log(`${sender}: ${message}`)
+    io.emit("new message", { sender, message })
   })
 }
