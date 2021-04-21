d50079709020863c5ae1f39ce32daa424774aa3b
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-21 15:47:09 -0700

    Integrate SocketIO

--- Class Work/make-chat/app.js
+++ Class Work/make-chat/app.js
@@ -2,14 +2,24 @@ const express = require("express")
 const app = express()
 const server = require("http").Server(app)
 
+const io = require("socket.io")(server)
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected!")
+})
+
+// Handlerbars
 const exphbs = require("express-handlebars")
 app.engine("handlebars", exphbs())
 app.set("view engine", "handlebars")
 
+app.use("/public", express.static("public"))
+
+// Routes
 app.get("/", (req, res) => {
   res.render("index.handlebars")
 })
 
+// Start listening
 server.listen(3000, () => {
   console.log("Server is up and running on port 3000.")
 })
