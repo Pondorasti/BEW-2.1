cd368a8590eef0d2167f5fd59079a62973faeeeb
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-28 21:36:34 -0700

    Add socket.io

--- Class Work/socket-chat/index.js
+++ Class Work/socket-chat/index.js
@@ -2,9 +2,15 @@ const express = require("express")
 const app = express()
 const httpServer = require("http")
 const server = httpServer.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html")
+})
+
+io.on("connection", (socket) => {
+  console.log("a user connected")
 })
 
 server.listen(3000, () => {
